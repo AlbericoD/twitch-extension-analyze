d2dd046637a337db03252ad890916a812d2a0f83
AlbericoD <a.diasbarreto@icloud.com>
2019-03-29 17:54:05 -0300

    Updates

---
+++ precache-manifest.04ae0b1a8132a130a5d9c7eb48624950.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/twitch-extension-analyze/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b3f7a8f1762cc2f9c785",
+    "url": "/twitch-extension-analyze/static/js/runtime~main.645e04e5.js"
+  },
+  {
+    "revision": "71d1168df0bf43159a03",
+    "url": "/twitch-extension-analyze/static/js/main.a3f528ea.chunk.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/twitch-extension-analyze/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "71d1168df0bf43159a03",
+    "url": "/twitch-extension-analyze/static/css/main.3d41eb91.chunk.css"
+  },
+  {
+    "revision": "1521448e5814011051e05a22f48c3f24",
+    "url": "/twitch-extension-analyze/index.html"
+  }
+];
