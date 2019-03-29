a1fb3fa4ff1a7ada1195def39f89b1edb41f693b
AlbericoD <a.diasbarreto@icloud.com>
2019-03-29 18:05:55 -0300

    add antd, react overrides

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
+import { Button } from 'antd';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
           <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'>
             Learn React
           </a>
+          <Button>Test AntD</Button>
         </header>
       </div>
     );
