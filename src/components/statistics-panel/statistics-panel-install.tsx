78007464f01c9ac4503367f8faa16f85330c9d5e
AlbericoD <a.diasbarreto@icloud.com>
2019-04-02 15:05:19 -0300

    data reflecting the calendar date range

--- src/components/statistics-panel/statistics-panel-install.tsx
+++ src/components/statistics-panel/statistics-panel-install.tsx
@@ -10,26 +10,39 @@ interface IProps {
 }
 export const StatisticsPanelInstall = ({ cardCss, statisticCss, csv, children }: IProps) => (
   <Row gutter={16}>
-    <Col span={12}>
+    <Col span={8}>
       <Card.Grid style={cardCss}>
         <Statistic
-          title='Activations (Total)'
-          value={csv.map(item => parseInt(item.Activations)).reduce((prev, next) => prev + next, 0)}
+          title='Details Page Visits'
+          value={csv
+            .map(item => parseInt(item['Extension Details Page Visits']))
+            .reduce((prev, next) => prev + next, 0)}
           valueStyle={{ color: '#b19dd8' }}
           precision={0}
           style={statisticCss}
         />
       </Card.Grid>
     </Col>
-    <Col span={12}>
+    <Col span={8}>
       <Card.Grid style={cardCss}>
         <Statistic
           valueStyle={{ color: '#b19dd8' }}
-          title='Uninstalls (Total)'
+          title='Uninstalls / Installs'
           value={csv.map(item => parseInt(item.Uninstalls)).reduce((prev, next) => prev + next, 0)}
           suffix={`/ ${csv
             .map(item => parseInt(item.Installs))
             .reduce((prev, next) => prev + next, 0)}`}
+          style={statisticCss}
+        />
+      </Card.Grid>
+    </Col>
+    <Col span={8}>
+      <Card.Grid style={cardCss}>
+        <Statistic
+          title='Activations'
+          value={csv.map(item => parseInt(item.Activations)).reduce((prev, next) => prev + next, 0)}
+          valueStyle={{ color: '#b19dd8' }}
+          precision={0}
           style={statisticCss}
         />
       </Card.Grid>
--- src/containers/installations-graph.tsx
+++ src/containers/installations-graph.tsx
@@ -1,8 +1,8 @@
-import React, { CSSProperties } from 'react';
+import React, { CSSProperties, FunctionComponent, useState, useEffect } from 'react';
 import { Card } from 'antd';
 import { StatisticsPanelInstall } from '../components/statistics-panel';
 import { StatisticInstallGraph } from '../components/graphs/install-statistic-graph';
-import { ITwitchExtensionPrimitiveCSV } from '../components';
+import { ITwitchExtensionPrimitiveCSV, IDataInstalls } from '../components';
 
 interface IProps {
   gridStyleStatisc: CSSProperties;
@@ -12,23 +12,54 @@ interface IProps {
   initialDateIndex: number;
   lastDateIndex: number;
 }
-export const InstallationsGraphBox = ({
-  gridStyleStatisc,
-  statisticStyle,
-  graphCardStyle,
-  csv,
-  initialDateIndex,
-  lastDateIndex
-}: IProps): JSX.Element => (
-  <Card style={graphCardStyle}>
-    <Card.Grid style={{ width: '100%', padding: 3 }}>
-      <StatisticsPanelInstall cardCss={gridStyleStatisc} statisticCss={statisticStyle} csv={csv}>
-        <StatisticInstallGraph
-          data={csv}
-          initialDateIndex={initialDateIndex}
-          lastDateIndex={lastDateIndex}
-        />
-      </StatisticsPanelInstall>
-    </Card.Grid>
-  </Card>
-);
+interface IContainerData {
+  installs: IDataInstalls[];
+  csv: ITwitchExtensionPrimitiveCSV[];
+}
+const makeData = (
+  csv: ITwitchExtensionPrimitiveCSV[],
+  initialDateIndex: number,
+  lastDateIndex: number
+): IContainerData => {
+  let installsData: IDataInstalls[] = [];
+  let csvData: ITwitchExtensionPrimitiveCSV[] = [];
+
+  csv.filter((item, index) => {
+    if (index >= lastDateIndex && index <= initialDateIndex) {
+      const install: IDataInstalls = {
+        name: item.Date,
+        activations: parseInt(item.Activations),
+        install: parseInt(item.Installs),
+        uninstalls: parseInt(item.Uninstalls)
+      };
+      installsData.push(install);
+      csvData.push(item);
+    }
+  });
+  installsData.reverse();
+  csvData.reverse();
+  return {
+    csv: csvData,
+    installs: installsData
+  };
+};
+
+export const InstallationsGraphBox: FunctionComponent<IProps> = (props: IProps): JSX.Element => {
+  const [data, setState] = useState<IContainerData>({ csv: [], installs: [] });
+  useEffect(() => {
+    setState(makeData(props.csv, props.initialDateIndex, props.lastDateIndex));
+  }, [props.csv, props.initialDateIndex, props.lastDateIndex]);
+
+  return (
+    <Card style={props.graphCardStyle}>
+      <Card.Grid style={{ width: '100%', padding: 3 }}>
+        <StatisticsPanelInstall
+          cardCss={props.gridStyleStatisc}
+          statisticCss={props.statisticStyle}
+          csv={data.csv}>
+          <StatisticInstallGraph data={data.installs} />
+        </StatisticsPanelInstall>
+      </Card.Grid>
+    </Card>
+  );
+};
