4c8fc550b2b773316ef70a8145cf75f86818f973
Albérico Dias Barreto Filho <AlbericoD@users.noreply.github.com>
2019-06-20 21:33:45 -0300

    Fix (#13)
    - corrected problem with date ranges between 15, 30 and 45 days, the function was rewritten to correct this problem

--- src/utils/make-ranges.ts
+++ src/utils/make-ranges.ts
@@ -7,114 +7,27 @@ const dateFormat = 'YYYY/MM/DD';
 interface IRange {
   [range: string]: RangePickerPresetRange;
 }
+
+const checkData = (data: ITwitchExtensionPrimitiveCSV[], length: number): boolean =>
+  data.length >= 0 && data.length >= length;
+
+const makeData = (dateInitial: string, dateFinal: string): RangePickerPresetRange => [
+  moment(dateInitial, dateFormat),
+  moment(dateFinal, dateFormat)
+];
+
 export const makeRanges = (data: ITwitchExtensionPrimitiveCSV[]): IRange => {
-  if (data.length >= 0 && data.length >= 120) {
-    return {
-      'Last Day': [moment(data[0].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 3 Days': [moment(data[3].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 7 Days': [moment(data[7].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 15 Days': [moment(data[15].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 30 Days': [moment(data[30].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 45 Days': [moment(data[45].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 60 Days': [moment(data[60].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 90 Days': [moment(data[90].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 120 Days': [moment(data[120].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'First Extension Release - Last Date CSV': [
-        moment(data[data.length - 1].Date, dateFormat),
-        moment(data[0].Date, dateFormat)
-      ]
-    };
-  } else if (data.length >= 0 && data.length >= 90) {
-    return {
-      'Last Day': [moment(data[0].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 3 Days': [moment(data[3].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 7 Days': [moment(data[7].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 15 Days': [moment(data[15].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 30 Days': [moment(data[30].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 45 Days': [moment(data[45].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 60 Days': [moment(data[60].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 90 Days': [moment(data[90].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'First Extension Release - Last Date CSV': [
-        moment(data[data.length - 1].Date, dateFormat),
-        moment(data[0].Date, dateFormat)
-      ]
-    };
-  } else if (data.length >= 0 && data.length >= 60) {
-    return {
-      'Last Day': [moment(data[0].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 3 Days': [moment(data[3].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 7 Days': [moment(data[7].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 15 Days': [moment(data[15].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 30 Days': [moment(data[30].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 45 Days': [moment(data[45].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 60 Days': [moment(data[60].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'First Extension Release - Last Date CSV': [
-        moment(data[data.length - 1].Date, dateFormat),
-        moment(data[0].Date, dateFormat)
-      ]
-    };
-  } else if (data.length >= 0 && data.length >= 45) {
-    return {
-      'Last Day': [moment(data[0].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 3 Days': [moment(data[3].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 7 Days': [moment(data[7].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 15 Days': [moment(data[15].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 30 Days': [moment(data[30].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 45 Days': [moment(data[45].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'First Extension Release - Last Date CSV': [
-        moment(data[data.length - 1].Date, dateFormat),
-        moment(data[0].Date, dateFormat)
-      ]
-    };
-  } else if (data.length >= 0 && data.length >= 30) {
-    return {
-      'Last Day': [moment(data[0].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 3 Days': [moment(data[3].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 7 Days': [moment(data[7].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 15 Days': [moment(data[15].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 30 Days': [moment(data[30].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'First Extension Release - Last Date CSV': [
-        moment(data[data.length - 1].Date, dateFormat),
-        moment(data[0].Date, dateFormat)
-      ]
-    };
-  } else if (data.length >= 0 && data.length >= 15) {
-    return {
-      'Last Day': [moment(data[0].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 3 Days': [moment(data[3].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 7 Days': [moment(data[7].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 15 Days': [moment(data[15].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'First Extension Release - Last Date CSV': [
-        moment(data[data.length - 1].Date, dateFormat),
-        moment(data[0].Date, dateFormat)
-      ]
-    };
-  } else if (data.length >= 0 && data.length >= 7) {
-    return {
-      'Last Day': [moment(data[0].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 3 Days': [moment(data[3].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 7 Days': [moment(data[7].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'First Extension Release - Last Date CSV': [
-        moment(data[data.length - 1].Date, dateFormat),
-        moment(data[0].Date, dateFormat)
-      ]
-    };
-  } else if (data.length >= 0 && data.length >= 3) {
-    return {
-      'Last Day': [moment(data[0].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'Last 3 Days': [moment(data[3].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'First Extension Release - Last Date CSV': [
-        moment(data[data.length - 1].Date, dateFormat),
-        moment(data[0].Date, dateFormat)
-      ]
-    };
-  } else {
-    return {
-      'Last Day': [moment(data[0].Date, dateFormat), moment(data[0].Date, dateFormat)],
-      'First Extension Release - Last Date CSV': [
-        moment(data[data.length - 1].Date, dateFormat),
-        moment(data[0].Date, dateFormat)
-      ]
-    };
-  }
+  const ranges: IRange = {
+    'Last Day': makeData(data[0].Date, data[0].Date)
+  };
+  const intervals = [3, 7, 15, 30, 45, 60, 90, 120, 180, 240];
+  intervals.forEach(interval => {
+    if (checkData(data, interval))
+      ranges[`Last ${interval} Days`] = makeData(data[interval - 1].Date, data[0].Date);
+  });
+  ranges['First Extension Release - Last Date CSV'] = makeData(
+    data[data.length - 1].Date,
+    data[0].Date
+  );
+  return ranges;
 };
